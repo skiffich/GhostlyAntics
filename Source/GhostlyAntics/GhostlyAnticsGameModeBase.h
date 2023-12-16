ffac110839101c7312e504d2df84b037554b7ce9
Dmytro <skiff120@gmail.com>
2023-12-16 18:39:51 +0200

    add blank project

---
+++ Source/GhostlyAntics/GhostlyAnticsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostlyAnticsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTLYANTICS_API AGhostlyAnticsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
